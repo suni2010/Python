8c44a619abfeedcf3984735ccfbf437aec089b72
suni2010 <90258867+suni2010@users.noreply.github.com>
2024-06-17 19:42:50 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,9 @@
+colour = input('Enter the valid colour:')
+if (colour=='red'):
+    print("please stop")
+elif (colour=='green'):
+    print('please go')
+elif (colour=='yellow'):
+    print('please wait')
+else:
+    print(colour,'colour is not available in the traffic light')
